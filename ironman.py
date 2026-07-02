5ab526af5beb24ad696f7da5710c3bc88e5541df
yuva charan <yuvacharanvps@gmail.com>
2026-07-02 20:37:08 +0530

    added feww lines of code in ironman.py in sub2 branch

--- ironman.py
+++ ironman.py
@@ -1,1 +1,17 @@
 #this is code to draw iron man face in python
+turtle.pendown()
+    turtle.color('red')
+    turtle.begin_fill()
+    for i in range(len(piece[0])):
+        x,y=piece[0][i]
+        turtle.goto(x,y)
+
+    for i in range(len(piece[1])):
+        x,y=piece[1][i]
+        turtle.goto(x,y)
+    turtle.end_fill()
+draw_piece(piece1,piece1Goto)
+draw_piece(piece2,piece2Goto)
+draw_piece(piece3,piece3Goto)
+turtle.hideturtle()
+turtle.done()
